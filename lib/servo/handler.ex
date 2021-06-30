591c0233f7015c07f1b1d5b486fe3bfd56a33898
Andrew Park <atpark333@gmail.com>
2021-06-30 23:49:43 +0900

    feature: Add status code and undefined routes

--- lib/servo/handler.ex
+++ lib/servo/handler.ex
@@ -16,30 +16,51 @@ defmodule Servo.Handler do
         |> List.first
         |> String.split(" ")
 
-    %{ method: method, path: path, resp_body: "" }
+    %{ method: method,
+       path: path,
+       resp_body: "",
+       status: nil }
   end
   
-  def route(%{method: "GET", path: "/bots"} = req) do
-    %{ req | resp_body: "Cambot, Gypsy, Tom Servo, Crooooooow" }
+  # Routes
+  def route(%{ method: "GET", path: "/bots" } = req) do
+    bots = ["Cambot", "Gypsy", "Tom Servo", "Croooooow"]
+    %{ req | status: 200, resp_body: Enum.join(bots, "\n") }
   end
 
-  def route(%{method: "GET", path: "/sirs"} = req) do
-    %{ req | resp_body: "Clayton Brown, TV's Frank" }
+  def route(%{ method: "GET", path: "/sirs" } = req) do
+    sirs = ["Dr. Clayton Brown", "TV's Frank"]
+    %{ req | status: 200, resp_body: Enum.join(sirs, "\n")}
+  end
+
+  def route(%{ method: method, path: path } = req) do
+    %{ req | status: 404, resp_body: "Cannot #{method} route #{path}" }
   end
   
   def format_response(req) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{req.status} #{status_desc(req.status)}
     Content-Type: text/html
     Content-Length: #{String.length(req.resp_body)} 
 
     #{req.resp_body}
     """
   end
+  
+  defp status_desc(code) do
+    %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error"
+    }[code]
+  end
 end
 
 request = """
-GET /bots HTTP/1.1
+GET /sirs HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
 Accept: */*
