27090a2a5cda2f0dedc546a23764d086340da944
Andrew Park <atpark333@gmail.com>
2021-07-14 20:09:28 +0900

    refactor: Rename props and functions

--- lib/servo/parser.ex
+++ lib/servo/parser.ex
@@ -6,20 +6,33 @@ defmodule Servo.Parser do
     [head, body] = String.split(request, "\n\n")
     
     # Separate request headers from request method line
-    [request_line | _headers] = String.split(head, "\n")
+    [request_line | header_lines] = String.split(head, "\n")
 
     [method, path, _] = String.split(request_line, " ")
     
-    params = parse_params(body)
+    headers = parse_headers(header_lines, %{})
+
+    params = parse_params(headers["Content-Type"], body)
 
     %Request{
       method: method,
       path: path,
-      params: params
+      params: params,
+      headers: headers
     }
   end
   
-  def parse_params(params_string) do
+  def parse_headers([h | t], headers) do
+    [key, value] = String.split(h, ":")
+    headers = Map.put(headers, key, value)
+    parse_headers(t)
+  end
+  
+  def parse_headers([], headers), do: headers
+  
+  def parse_params(_, _), do: %{}
+  
+  def parse_params("application/x-www-form-urlencoded", params_string) do
     params_string |> String.trim |> URI.decode_query
   end
 end
