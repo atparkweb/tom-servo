4240d87d737d58c609a4ee960b0a57b145429e70
Andrew Park <atpark333@gmail.com>
2021-07-25 18:22:08 +0900

    feature: Add store for mock data

---
+++ lib/servo/bot_store.ex
@@ -0,0 +1,33 @@
+defmodule Servo.BotStore do
+  @moduledoc """
+  Mock data store to test with. To be replaced with backing service
+
+  """
+
+  alias Servo.Bot
+  
+  @doc """
+  Get a list of all the Bots
+  """
+  def list_bots do
+    [
+      %Bot{ id: 1, name: "Crow", color: "Yellow"},
+      %Bot{ id: 2, name: "Tom Servo", color: "Red"},
+      %Bot{ id: 3, name: "Gypsy", color: "Purple"},
+      %Bot{ id: 4, name: "Cambot", color: "Orange"}
+    ]
+  end
+  
+  @doc """
+  Get a single bot by ID
+  """
+  def get_bot(id) when is_integer(id) do
+    Enum.find(list_bots(), fn(b) -> b.id == id end)
+  end
+  
+  def get_bot(id) when is_binary(id) do
+    id
+    |> String.to_integer
+    |> get_bot
+  end
+end
