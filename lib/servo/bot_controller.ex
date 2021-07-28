10b8a159a9505067d6f618df669eed74bb787ed7
Andrew Park <atpark333@gmail.com>
2021-07-29 00:08:57 +0900

    refactor: Move rendering logic to a View module

--- lib/servo/bot_controller.ex
+++ lib/servo/bot_controller.ex
@@ -1,32 +1,24 @@
 defmodule Servo.BotController do
   alias Servo.Bot
   alias Servo.BotStore
+  alias Servo.BotView
   alias Servo.Request
   
-  @templates_path Path.expand("../../templates", __DIR__)
-  
-  defp render(req, template, bindings \\ []) do
-    content =
-      @templates_path
-      |> Path.join(template) 
-      |> EEx.eval_file(bindings)
-
-    %Request{ req | status: 200, res_body: content }
-  end
-
   def index(req) do
     bot_list =
       BotStore.list_bots
       |> Enum.filter(fn(b) -> b.is_active end)
       |> Enum.sort(&Bot.order_by_name_asc/2) # sort alphabetically by name
 
-    render(req, "index.eex", bots: bot_list)
+    # render(req, "index.eex", bots: bot_list)
+    %Request{ req | status: 200, res_body: BotView.index(bot_list) }
   end
 
   def show(req, %{ "id" => id }) do
     bot = BotStore.get_bot(id)
     
-    render(req, "show.eex", bot: bot)
+    # render(req, "show.eex", bot: bot)
+    %Request{ req | status: 200, res_body: BotView.show(bot) }
   end
 
   def create(req, %{ "name" => name, "color" => color }) do
