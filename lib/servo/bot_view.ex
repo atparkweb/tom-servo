e86f6f38468c238718f08964439807eb10e7b896
Andrew Park <atpark333@gmail.com>
2021-07-29 11:18:34 +0900

    fix: Comment out unused render function

--- lib/servo/bot_view.ex
+++ lib/servo/bot_view.ex
@@ -1,20 +1,17 @@
 defmodule Servo.BotView do
   require EEx
 
-  alias Servo.Request
-  
   @templates_path Path.expand("../../templates", __DIR__)
   
   EEx.function_from_file :def, :index, Path.join(@templates_path, "index.eex"), [:bots]
   EEx.function_from_file :def, :show, Path.join(@templates_path, "show.eex"), [:bot]
   
-  defp render(req, template, bindings \\ []) do
-    content =
-      @templates_path
-      |> Path.join(template) 
-      |> EEx.eval_file(bindings)
+  # defp render(req, template, bindings \\ []) do
+  #   content =
+  #     @templates_path
+  #     |> Path.join(template) 
+  #     |> EEx.eval_file(bindings)
 
-    %Request{ req | status: 200, res_body: content }
-  end
-
+  #   %Request{ req | status: 200, res_body: content }
+  # end
 end
