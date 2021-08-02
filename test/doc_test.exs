92bd6902c37cb797aac62a9cbff8738f22193013
Andrew Park <atpark333@gmail.com>
2021-08-02 22:52:18 +0900

    refactor: Move doctests to separate file

---
+++ test/doc_test.exs
@@ -0,0 +1,5 @@
+defmodule DocTest do
+  use ExUnit.Case
+  doctest Servo.Parser
+  doctest Servo.Handler
+end
--- test/handler_test.exs
+++ test/handler_test.exs
@@ -1,5 +1,5 @@
 defmodule HandlerTest do
-  use ExUnit.Case
+  use ExUnit.Case, async: true
   
   import Servo.Handler, only: [handle: 1]
   
