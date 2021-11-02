1cd6ba4d18bac3ead083e314b7dd42b7730c2983
Andrew Park <atpark333@gmail.com>
2021-11-02 09:59:20 +0900

    refactor(http_server): update test to check multiple URLs

--- test/http_server_test.exs
+++ test/http_server_test.exs
@@ -6,16 +6,20 @@ defmodule HttpServerTest do
   test "accepts a request on a socket and sends back a response" do
     spawn(HttpServer, :start, [4000])
     
-    url = "http://localhost:4000/bot_crew"
+    urls = [
+      "http://localhost:4000/bot_crew",
+      "http://localhost:4000/bots",
+      "http://localhost:4000/bots/1",
+      "http://localhost:4000/api/bots"
+    ]
     
-    1..5
-    |> Enum.map(fn(_) -> Task.async(fn -> HTTPoison.get(url) end) end)
+    urls
+    |> Enum.map(fn(url) -> Task.async(fn -> HTTPoison.get(url) end) end)
     |> Enum.map(&Task.await/1)
     |> Enum.map(&assert_successful_response/1)
   end
   
   defp assert_successful_response({:ok, response}) do
     assert response.status_code == 200
-    assert response.body == "Cambot, Gypsy, Tom Servo, Crow"
   end
 end
