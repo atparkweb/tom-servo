24ecbb9428993bf2ffaa27769f74df5151828dfe
Andrew Park <atpark333@gmail.com>
2021-11-21 13:00:41 +0900

    fix: set correct request params

--- lib/servo/controllers/message_controller.ex
+++ lib/servo/controllers/message_controller.ex
@@ -3,14 +3,14 @@ defmodule Servo.Controllers.MessageController do
 
   def index(req) do
     messages = MessageServer.recent_messages()
-    %{ req | state: 200, resp_body: (inspect messages) }
+    %{ req | status: 200, res_body: (inspect messages) }
   end
   
   def create(req, %{"name" => name, "message" => message}) do
     MessageServer.create_message(name, message)
     receive do
       {:create_message, name, message} ->
-        %{ req | status: 201, resp_body: "#{name}: #{message}" }
+        %{ req | status: 201, res_body: "#{name}: #{message}" }
     end
   end
 end
