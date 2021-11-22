9a6937802e12884e4f253b15ea2c6ea8edf9007f
Andrew Park <atpark333@gmail.com>
2021-11-22 22:23:42 +0900

    feat: add total messages handler and catch all

--- lib/servo/message_server.ex
+++ lib/servo/message_server.ex
@@ -8,23 +8,6 @@ defmodule Servo.MessageServer do
     pid
   end
 
-  @doc "Server process loop. State should initially be an empty list."
-  def listen_loop(state) do
-    receive do
-      {pid, :create_message, name, message} ->
-        {:ok, id} = save_message(name, message)
-        
-	# only track last 3 messages
-        new_state = [ {name, message} | Enum.take(state, 2) ]
-
-	send pid, {:response, id}
-        listen_loop(new_state)
-      {pid, :recent_messages} ->
-        send pid, {:response, state}
-        listen_loop(state)
-    end
-  end
-
   def recent_messages do
     send @process, {self(), :recent_messages}
     receive do {:response, messages} -> messages end
@@ -38,6 +21,30 @@ defmodule Servo.MessageServer do
   defp save_message(name, _message) do
     # TODO: Send to backing service
     { :ok, "message from #{name} " }
+  end
+
+  @doc "Server process loop. State should initially be an empty list."
+  def listen(state) do
+    receive do
+      {pid, :create_message, name, message} ->
+        {:ok, id} = save_message(name, message)
+        
+	# only track last 3 messages
+        new_state = [ {name, message} | Enum.take(state, 2) ]
+
+	send pid, {:response, id}
+        listen_loop(new_state)
+      {pid, :recent_messages} ->
+        send pid, {:response, state}
+        listen_loop(state)
+      {pid, :total_messages} ->
+        total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+	send pid, {:response, total}
+	listen_loop(state)
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+	listen_loop(state)
+    end
   end
 end
 
