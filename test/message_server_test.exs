62d819ca66599b2901eb83fff3d56787589680fe
Andrew Park <atpark333@gmail.com>
2021-11-23 22:07:43 +0900

    test: add message server test

---
+++ test/message_server_test.exs
@@ -0,0 +1,24 @@
+defmodule MessageServerTest do
+  use ExUnit.Case
+  
+  alias Servo.MessageServer
+  
+  test "should cache the 3 most recent messages" do
+    MessageServer.start()
+
+    MessageServer.create_message("user1", "hello")
+    MessageServer.create_message("user2", "hi")
+    MessageServer.create_message("user3", "aloha")
+    MessageServer.create_message("user1", "goodbye")
+    MessageServer.create_message("user2", "bye")
+    MessageServer.create_message("user3", "aloha")
+    
+    recent = [
+      {"user3", "aloha"},
+      {"user2", "bye"},
+      {"user1", "goodbye"}
+    ]
+    
+    assert MessageServer.recent_messages() == recent
+  end
+end
