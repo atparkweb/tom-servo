aff2920e0ac9a00c0b928f4dcd98e28b648bcb11
Andrew Park <atpark333@gmail.com>
2021-12-23 21:11:27 +0900

    test: add test for 404 reset function

--- test/four_oh_four_counter_test.exs
+++ test/four_oh_four_counter_test.exs
@@ -3,18 +3,38 @@ defmodule FourOhFourCounterTest do
 
   alias Servo.FourOhFourCounter, as: Counter
 
-  test "reports counts of missing path requests" do
-    Counter.start()
+  describe "counts 404 requests" do
 
-    Counter.inc("/terminator")
-    Counter.inc("/robocop")
-    Counter.inc("/robocop")
-    Counter.inc("/terminator")
-    Counter.inc("/robocop")
+    setup do
+      pid = Counter.start()
+      on_exit(fn ->
+        Process.exit(pid, :kill)
+      end)
+      {:ok, counter: Counter, pid: pid}
+    end
 
-    assert Counter.get_count("/robocop") == 3
-    assert Counter.get_count("/terminator") == 2
+    test "reports counts of missing path requests" do
+      Counter.inc("/terminator")
+      Counter.inc("/robocop")
+      Counter.inc("/robocop")
+      Counter.inc("/terminator")
+      Counter.inc("/robocop")
 
-    assert Counter.get_counts == %{ "/terminator" => 2, "/robocop" => 3 }
+      assert Counter.get_count("/robocop") == 3
+      assert Counter.get_count("/terminator") == 2
+
+      assert Counter.get_counts == %{ "/terminator" => 2, "/robocop" => 3 }
+    end
+
+    test "resets counts" do
+      Counter.inc("/killroy")
+      Counter.inc("/killroy")
+      Counter.inc("/robo")
+      Counter.inc("/robo")
+      assert Counter.get_counts == %{ "/robo" => 2, "/killroy" => 2 }
+
+      Counter.reset()
+      assert Counter.get_counts == %{}
+    end
   end
 end
