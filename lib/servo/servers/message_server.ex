749cf751b1961a50aca15f5dfa2540f58d076d38
Andrew Park <atpark333@gmail.com>
2021-12-23 12:09:05 +0900

    feat: implement generic server

--- lib/servo/servers/message_server.ex
+++ lib/servo/servers/message_server.ex
@@ -1,46 +1,67 @@
-defmodule Servo.MessageServer do
-  @process __MODULE__
-
-  def start do
-    IO.puts "Starting message server..."
-    pid = spawn(__MODULE__, :listen_loop, [[]])
-    Process.register(pid, @process)
+defmodule Servo.GenericServer do
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
     pid
   end
 
+  def call(pid, message) do
+    send pid, {:call, self(), message}
+    receive do {:response, response} -> response end
+  end
+
+  # for requests that don't expect a response
+  def cast(pid, message) do
+    send pid, {:cast, message}
+  end
+
+  @doc "Server process loop. State should initially be an empty list."
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send sender, {:response, response}
+        listen_loop(new_state, callback_module)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+        listen_loop(state, callback_module)
+    end
+  end
+
+end
+
+defmodule Servo.MessageServer do
+  @name :message_server
+
+  alias Servo.GenericServer
+
+  def start do
+    IO.puts "Starting the message server..."
+    GenericServer.start(__MODULE__, [], @name)
+  end
+
   def recent_messages do
-    call @process, :recent_messages
+    GenericServer.call @name, :recent_messages
   end
 
   def total_messages do
-    call @process, :total_messages
+    GenericServer.call @name, :total_messages
+  end
+
+  def clear do
+    GenericServer.cast @name, :clear
   end
 
   def create_message(name, message) do
-    call @process, {:create_message, name, message} 
-  end
-
-  def call(pid, message) do
-    send pid, {self(), message}
-    receive do {:response, response} -> response end
+    GenericServer.call @name, {:create_message, name, message} 
   end
 
   defp save_message(name, _message) do
     # TODO: Send to backing service
     { :ok, "message from #{name} " }
-  end
-
-  @doc "Server process loop. State should initially be an empty list."
-  def listen_loop(state) do
-    receive do
-      {sender, message} when is_pid(sender) ->
-        {response, new_state} = handle_call(message, state)
-        send sender, {:response, response}
-        listen_loop(new_state)
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect unexpected}"
-        listen_loop(state)
-    end
   end
 
   def handle_call(:total_messages, state) do
@@ -57,6 +78,9 @@ defmodule Servo.MessageServer do
     most_recent = Enum.take(state, 2)
     new_state = [ {name, message} | most_recent ]
     {id, new_state}
+  end
+
+  def handle_cast(:clear, _state) do
   end
 end
 
