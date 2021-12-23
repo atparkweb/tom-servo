61c867c6c9dfa7e8bc2ea4ad61cf23f1b541c61d
Andrew Park <atpark333@gmail.com>
2021-12-23 22:57:27 +0900

    refactor: change message_server to use built in GenServer

--- lib/servo/servers/message_server.ex
+++ lib/servo/servers/message_server.ex
@@ -1,62 +1,35 @@
-defmodule Servo.GenericServer do
-  def start(callback_module, initial_state, name) do
-    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
-    Process.register(pid, name)
-    pid
-  end
-
-  def call(pid, message) do
-    send pid, {:call, self(), message}
-    receive do {:response, response} -> response end
-  end
-
-  # for requests that don't expect a response
-  def cast(pid, message) do
-    send pid, {:cast, message}
-  end
-
-  @doc "Server process loop. State should initially be an empty list."
-  def listen_loop(state, callback_module) do
-    receive do
-      {:call, sender, message} when is_pid(sender) ->
-        {response, new_state} = callback_module.handle_call(message, state)
-        send sender, {:response, response}
-        listen_loop(new_state, callback_module)
-      {:cast, message} ->
-        new_state = callback_module.handle_cast(message, state)
-        listen_loop(new_state, callback_module)
-      unexpected ->
-        IO.puts "Unexpected message: #{inspect unexpected}"
-        listen_loop(state, callback_module)
-    end
-  end
-
-end
-
 defmodule Servo.MessageServer do
   @name :message_server
 
-  alias Servo.GenericServer
+  use GenServer
+
+  defmodule State do
+    defstruct cache_size: 3, messages: []
+  end
 
   def start do
     IO.puts "Starting the message server..."
-    GenericServer.start(__MODULE__, [], @name)
+    GenServer.start(__MODULE__, %State{}, name: @name)
   end
 
   def recent_messages do
-    GenericServer.call @name, :recent_messages
+    GenServer.call @name, :recent_messages
   end
 
   def total_messages do
-    GenericServer.call @name, :total_messages
+    GenServer.call @name, :total_messages
   end
 
   def clear do
-    GenericServer.cast @name, :clear
+    GenServer.cast @name, :clear
   end
 
   def create_message(name, message) do
-    GenericServer.call @name, {:create_message, name, message} 
+    GenServer.call @name, {:create_message, name, message} 
+  end
+
+  def set_cache_size(size) do
+    GenServer.cast @name, {:set_cache_size, size}
   end
 
   defp save_message(name, _message) do
@@ -64,24 +37,38 @@ defmodule Servo.MessageServer do
     { :ok, "message from #{name} " }
   end
 
-  def handle_call(:total_messages, state) do
-    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
-    {total, state}
+  def handle_call(:total_messages, _from, state) do
+    total = Enum.count(state.messages)
+    {:reply, total, state}
   end
-  def handle_call(:recent_messages, state) do
-    {state, state}
+  def handle_call(:recent_messages, _from, state) do
+    {:reply, state, state}
   end
-  def handle_call({:create_message, name, message}, state) do
+  def handle_call({:create_message, name, message}, _from, state) do
     {:ok, id} = save_message(name, message)
 
     # last 3 messages
-    most_recent = Enum.take(state, 2)
-    new_state = [ {name, message} | most_recent ]
-    {id, new_state}
+    most_recent = Enum.take(state.messages, state.cache_size - 1)
+    cached_messages = [ {name, message} | most_recent ]
+    new_state = %{ state | messages: cached_messages }
+    {:reply, id, new_state}
   end
 
-  def handle_cast(:clear, _state) do
-    []
+  def handle_cast(:clear, state) do
+    {:noreply, %{ state | messages: [] }}
+  end
+  def handle_cast({:set_cache_size, size}, state) do
+    new_state = %{ state | cache_size: size }
+    {:noreply, new_state}
   end
 end
 
+alias Servo.MessageServer
+
+MessageServer.start()
+MessageServer.create_message("Player 1", "Hello")
+MessageServer.create_message("Player 2", "Hi")
+MessageServer.create_message("Player 1", "What's up?")
+MessageServer.create_message("Player 3", "Hola")
+IO.inspect MessageServer.recent_messages()
+IO.inspect MessageServer.total_messages()
---
+++ lib/servo/servers/message_server_legacy.ex
@@ -0,0 +1,87 @@
+defmodule Servo.GenericServer do
+  def start(callback_module, initial_state, name) do
+    pid = spawn(__MODULE__, :listen_loop, [initial_state, callback_module])
+    Process.register(pid, name)
+    pid
+  end
+
+  def call(pid, message) do
+    send pid, {:call, self(), message}
+    receive do {:response, response} -> response end
+  end
+
+  # for requests that don't expect a response
+  def cast(pid, message) do
+    send pid, {:cast, message}
+  end
+
+  @doc "Server process loop. State should initially be an empty list."
+  def listen_loop(state, callback_module) do
+    receive do
+      {:call, sender, message} when is_pid(sender) ->
+        {response, new_state} = callback_module.handle_call(message, state)
+        send sender, {:response, response}
+        listen_loop(new_state, callback_module)
+      {:cast, message} ->
+        new_state = callback_module.handle_cast(message, state)
+        listen_loop(new_state, callback_module)
+      unexpected ->
+        IO.puts "Unexpected message: #{inspect unexpected}"
+        listen_loop(state, callback_module)
+    end
+  end
+
+end
+
+defmodule Servo.MessageServerLegacy do
+  @name :message_server_legacy
+
+  alias Servo.GenericServer
+
+  def start do
+    IO.puts "Starting the message server..."
+    GenericServer.start(__MODULE__, [], @name)
+  end
+
+  def recent_messages do
+    GenericServer.call @name, :recent_messages
+  end
+
+  def total_messages do
+    GenericServer.call @name, :total_messages
+  end
+
+  def clear do
+    GenericServer.cast @name, :clear
+  end
+
+  def create_message(name, message) do
+    GenericServer.call @name, {:create_message, name, message} 
+  end
+
+  defp save_message(name, _message) do
+    # TODO: Send to backing service
+    { :ok, "message from #{name} " }
+  end
+
+  def handle_call(:total_messages, state) do
+    total = Enum.map(state, &elem(&1, 1)) |> Enum.sum
+    {total, state}
+  end
+  def handle_call(:recent_messages, state) do
+    {state, state}
+  end
+  def handle_call({:create_message, name, message}, state) do
+    {:ok, id} = save_message(name, message)
+
+    # last 3 messages
+    most_recent = Enum.take(state, 2)
+    new_state = [ {name, message} | most_recent ]
+    {id, new_state}
+  end
+
+  def handle_cast(:clear, _state) do
+    []
+  end
+end
+
