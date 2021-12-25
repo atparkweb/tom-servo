4884c63e23489968aa5a5faada778783b4d7a92d
Andrew Park <atpark333@gmail.com>
2021-12-25 14:48:03 +0900

    refactor: add get_server client interface function

--- lib/servo/kick_starter.ex
+++ lib/servo/kick_starter.ex
@@ -1,9 +1,15 @@
 defmodule Servo.KickStarter do
+  @port 4000
+
   use GenServer
 
   def start do
     IO.puts "Starting kickstarter..."
     GenServer.start(__MODULE__, :ok, name: __MODULE__)
+  end
+
+  def get_server do
+    GenServer.call __MODULE__, :get_server
   end
 
   @impl true
@@ -14,15 +20,19 @@ defmodule Servo.KickStarter do
   end
 
   @impl true
-  def handle_info(message, _state) do
-    IO.puts "CRASH: #{inspect message}"
+  def handle_call(:get_server, _from, state) do
+    {:reply, state, state}
+  end
+
+  @impl true
+  def handle_info({:EXIT, _pid, reason}, _state) do
+    IO.puts "HttpServer exited: (#{inspect reason})"
     server_pid = start_server()
     {:noreply, server_pid}
   end
 
   defp start_server do
     IO.puts "Starting the HTTP server..."
-    server_pid = spawn_link(Servo.Servers.HttpServer, :start, [4000])
-    Process.register(server_pid, :http_server)
+    spawn_link(Servo.Servers.HttpServer, :start, [@port])
   end
 end
